fd2b5ba9ba5a05731e0c061557776477a94e43d2
Deep Singhvi <dsinghvi@umich.edu>
2021-08-27 13:14:22 -0400

    start freelancer view

--- src/tasks/ProposedTasks.tsx
+++ src/tasks/ProposedTasks.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { HTMLTable } from "@blueprintjs/core";
+import { Button, HTMLTable, Intent } from "@blueprintjs/core";
 import { Task } from "../App";
 
 
+const styles = {
+    buttonMargin: {
+        marginRight: '10px',
+    }
+}
 interface ProposedTaskProps {
+    isClientView: boolean;
     proposedTasks: Array<Task>;
 }
 
@@ -30,4 +36,59 @@ export function ProposedTasks(props: ProposedTaskProps) {
                 </tfoot>
             </HTMLTable>
         </div>);
+}
+
+function renderProposedTasks(props: ProposedTaskProps) {
+    if (props.isClientView) {
+        return (
+            <HTMLTable bordered={true} interactive={true} striped={true}>
+                <thead>
+                    <tr>
+                    <th>Task Description</th>
+                    <th>Task Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.proposedTasks.map(proposedTask => 
+                        <tr>
+                            <td>{proposedTask.taskDescription}</td>
+                            <td>{proposedTask.taskPrice}</td>
+                        </tr>
+                    )}
+                </tbody>
+                <tfoot>
+                </tfoot>
+            </HTMLTable>
+        );
+    } else {
+        return (
+            <HTMLTable bordered={true} interactive={true} striped={true}>
+                <thead>
+                    <tr>
+                    <th>Task Description</th>
+                    <th>Task Price</th>
+                    <th>Would you like to accept ?</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.proposedTasks.map(proposedTask => 
+                        <tr>
+                            <td>{proposedTask.taskDescription}</td>
+                            <td>{proposedTask.taskPrice}</td>
+                            <td>
+                                <Button 
+                                    style={styles.buttonMargin}
+                                    intent={Intent.SUCCESS} 
+                                    onClick={() => console.log("Accept task")}>
+                                    Accept Task
+                                </Button> 
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+                <tfoot>
+                </tfoot>
+            </HTMLTable>
+        )
+    }
 }
