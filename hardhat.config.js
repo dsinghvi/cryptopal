16b38e0642e8d732e1fc08b689eecf20902bd858
Deep Singhvi <dsinghvi@umich.edu>
2021-08-28 10:56:23 -0400

    fix hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,7 @@ const TEST_NETWORK = "TEST_NETWORK"
 const LOCAL_NETWORK = "LOCAL_NETWORK"
 
 // By default network is set to local, change it to TEST_NETWORK to make a switch
-const NETWORK = LOCAL_NETWORK
+const NETWORK = TEST_NETWORK
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
@@ -18,7 +18,7 @@ const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
 let networks = {};
 if (NETWORK === TEST_NETWORK) {
   networks = {
-    rinkeby: {
+    test_network: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
       accounts: [`0x${WALLET_PRIVATE_KEY}`]
     }
